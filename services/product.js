5053fcde937e6e2cb4f23edd85cfb7e9d15e5aef
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2023-07-26 17:53:17 +0300

    sorting and fields

--- services/product.js
+++ services/product.js
@@ -5,21 +5,42 @@ const Product = require("../models/product");
 const ApiError = require("../utils/apiError");
 
 exports.getProducts = asyncHandler(async (req, res) => {
+  //filtering
   const queryStringObject = { ...req.query };
   const excludesFields = ["page", "limit", "sort", "fields"];
   excludesFields.forEach((field) => delete queryStringObject[field]);
 
-   // Apply filtration using [gte, gt, lte, lt]
+  // Apply filtration using [gte, gt, lte, lt]
   let queryStr = JSON.stringify(queryStringObject);
   queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
+  //pagination
   const page = req.query.page * 1 || 1;
   const limit = req.query.limit * 1 || 10;
   const skip = (page - 1) * limit;
-  const products = await Product.find(JSON.parse(queryStr))
+  let mongoQuery = Product.find(JSON.parse(queryStr))
     .skip(skip)
     .limit(limit)
     .populate({ path: "category", select: "name" });
+
+  //sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    mongoQuery = mongoQuery.sort(sortBy);
+  } else {
+    mongoQuery = mongoQuery.sort("-createAt");
+  }
+
+  //fields
+  if(req.query.fields){
+    const fields = req.query.fields.split(",").join(" ");
+    mongoQuery = mongoQuery.select(fields);
+  }else{
+    mongoQuery = mongoQuery.select('-__v');
+  }
+
+  const products = await mongoQuery;
+
   res.status(200).json({ results: products.length, page, data: products });
 });
 
